32bb95bdd07b6080910772095593021a33ddc8e9
Thomas Prévost <thomasprevost85@gmail.com>
2023-03-20 09:25:30 +0100

    BE2.1

--- BE2/genCode.m
+++ BE2/genCode.m
@@ -7,8 +7,14 @@
 function C = genCode(G)
     [m,n] = size(G);
     C = zeros(2^n,n);
+    fprintf('Generating code matrix of size %d x %d\n',2^n,n);
     for i = 0:2^n-1
+        % progress bar
+        if mod(i,100) == 0
+            fprintf('Progress: %d/%d\n',i,2^n-1);
+        end
         b = de2bi(i,n);
         C(i+1,:) = mod(b*G(1),2);
     end
+    fprintf('Progress: %d/%d\n',2^n-1,2^n-1);
 end
--- BE2/parityCode.m
+++ BE2/parityCode.m
@@ -3,15 +3,9 @@
 % Generates a parity code given its parameters
 % Input: n - number of bits
 %        k - number of information bits
-% Output: H - parity code matrix
-%         G - generator matrix
+% Output: C - parity code matrix
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [H,G] = parityCode(n,k)
-    % cols of H are binary numbers from 1 to n written on n-k bits
-    H = zeros(n,n-k);
-    for i = 1:n-k
-        H(:,i) = str2num(dec2bin(i,n))';
-    end
-    % G is the matrix obtained by adding to H the identity matrix
-    G = [eye(n) H];
+function [C, G] = parityCode(n,k)
+    G = [eye(k) zeros(k,n-k)];
+    C = [G rem(G*G',2)];
 end
