f0825bdef4ba2a71c9fdc9d3e7ef084728101209
Thomas Prévost <thomasprevost85@gmail.com>
2023-03-20 10:06:02 +0100

    trying to fix Hamming

--- BE2/hammingCode.m
+++ BE2/hammingCode.m
@@ -7,13 +7,10 @@
 %         G - generator matrix
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [H,G] = hammingCode(n,k)
-    % cols of H are binary numbers from 1 to n written on n-k bits
-    H = zeros(n,n-k);
+    A = zeros(n-k, k);
     for i = 1:n-k
-        H(:,i) = str2num(dec2bin(i,n))';
+        A(i,:) = de2bi(i,k);
     end
-    % G is the matrix obtained by adding to H the identity matrix
-    G = [eye(n) H];
-    % H is the matrix obtained by adding to G the parity check matrix
+    G = [eye(k) A.'];
     H = [G rem(G*G',2)];
 end
